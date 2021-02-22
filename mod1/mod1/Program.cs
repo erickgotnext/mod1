36b200bb0b60bd468995538484e76fd7bea074bb
Erick Meraz <emeraz6747@southwestuniversity.edu>
2021-02-22 00:15:43 -0700

    mod1

---
+++ mod1/mod1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace mod1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        int a = SumOfThreeVariables(500, 200, 300);
+
+        Console.WriteLine(a);
+        }
+    static int SumOfThreeVariables(int value1, int value2, int value3)
+    {
+        int result = 0;
+
+        result = value1 + value2 + value3;
+
+        return result;
+    }
+}
+}
